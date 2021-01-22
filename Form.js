9b498ddd5c57e699f558ac1e7f1d4088bc41f5a6
AtharvaDadhich <72510024+AtharvaDadhich@users.noreply.github.com>
2021-01-22 17:49:24 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,51 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+    this.title = createElement('h2')
+    this.reset=createButton('reset')
+    this.leaderBoard=createElement('h1')
+  }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+    this.title.hide()
+  }
+
+  display(){
+    
+    this.title.html("Car Racing Game");
+    this.title.position(windowWidth/2-50,0);
+    
+    this.input.position(windowWidth/2-40,windowHeight/2-80);
+    this.button.position(windowWidth/2+30, windowHeight/2);
+    this.reset.position(windowWidth-100,20)
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(windowWidth/2-70,windowHeight/4);
+    });
+    this.reset.mousePressed(()=>{
+      player.updateCount(0)
+      game.update(0)
+      Player.updateFinish(0)
+    })
+  
+  }
+  leaderBoard(a,b){
+    var contain=createElement("div")
+    contain.position(0,0)
+    this.leaderBoard.id("lb")
+    this.leaderBoard.html(a+" is at "+b)
+    this.leaderBoard.position(windowWidth/2-50, windowHeight/4)
+   }
+}
